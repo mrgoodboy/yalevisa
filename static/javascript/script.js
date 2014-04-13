3a1ac258b392600f2ff9b068915dd6fb5f8e305a
Minh Tri Pham <minhtri@pham.cz>
2014-04-12 22:11:06 -0400

    some pics

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -84,7 +84,7 @@
 			$(this).qtip({ 
 				content: { 
 					title: '<h2>Kevin Nguyen</h2>',
-					text: buildprofile('/static/img/dance.jpg', 'Hello friends! <br><br> I’m Kevin (P.), ES ‘16, and I hail from the wild realm of Ohio, from a suburb devoid of much cultural diversity. When I’m not doing Asian things, you can find me playing in DPops and SYO, raising roundworms at the School of Medicine, crying over Kdramas, or getting fat from baked goods and ice cream. I also go to school.' + 
+					text: buildprofile('/static/img/poppics/kevin.jpg', 'Hello friends! <br><br> I’m Kevin (P.), ES ‘16, and I hail from the wild realm of Ohio, from a suburb devoid of much cultural diversity. When I’m not doing Asian things, you can find me playing in DPops and SYO, raising roundworms at the School of Medicine, crying over Kdramas, or getting fat from baked goods and ice cream. I also go to school.' + 
 						'<br><br>I got involved with ViSA last year to make up for this lack of culture in my life, and to obey my mother by making lots of “nice Vietnamese friends”. After a term as social chair, I now humbly serve as co-president of the most beautiful and talented cultural group on campus. I hope you’ll join me and the rest of ViSA in having a fun and delicious year!' +
 						'<br><br>See you at our next event (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧'
 						+'<hr>Email me at <a href="mailto:kevin.nguyen@yale.edu">kevin.nguyen@yale.edu</a> '),
@@ -113,14 +113,9 @@ effect: function(offset) {
 	$(this).qtip({ 
 		content: { 
 			title: '<h2>Jenny Nguyen</h2>',
-			text: buildprofile('/static/img/dance.jpg', 'As the first half of Team JK,\
-				Jenny is excited to push ViSA to greater heights this year and continue the traditions upheld by all the previous presidents!<br><br>\
-			    		Jenny is a proud member of the Jonathan Edwards class of 2016. Hailing from Canada (but living in NYC), she enjoys watching hockey,\
-			    		 building snowmen, being enthused about the simple pleasures of life, shopping, playing musical instruments, singing, dancing, frolicking about,\
-			    		  and exploring new places! <br><br>\
-			    		  Interested in public health, chemistry, French, and creative writing,\
-			    		   Jenny aspires to channel these passions into a future career as a doctor who might make educational scientific/health-related music videos to pass some knowledge onto her patients.\
-			    		    She loves meeting and welcoming new people, especially freshmen, so do not hesitate to reach out!'
+			text: buildprofile('/static/img/poppics/jenny.png', 'Hi there!\
+				<br><br>My name is Jenny, and I am a proud member of the Jonathan Edwards class of 2016. Hailing from Canada (but living in NYC), I enjoy watching hockey,	 building snowmen, being enthused about everything, shopping, playing musical instruments, singing, dancing, frolicking about,	 and exploring new places. As the first half of Team JK, I am excited to push ViSA to greater heights this year and continue the traditions upheld by our predecessors!\
+				<br><br>Interested in public health, chemistry, French, and creative writing, I aspire to channel these passions into a future career as a doctor who might make educational scientific/health-related music videos to pass some knowledge onto my patients. I love meeting and welcoming new people, especially freshmen, so do not hesitate to reach out!'
 				+'<hr>Email me at <a href="mailto:jenny.nguyen@yale.edu">jenny.nguyen@yale.edu</a> '),
 			button: true
 		},
@@ -146,7 +141,7 @@ effect: function(offset) {
 	$(this).qtip({ 
 		content: { 
 			title: '<h2>Mark Pham</h2>',
-			text: buildprofile('/static/img/dance.jpg', 'Hi guys! <br><br>' + 
+			text: buildprofile('/static/img/poppics/mark.jpg', 'Hi guys! <br><br>' + 
 				'I hail from the beautiful suburb of Springfield, Virginia, where I moved to from Saigon five years ago.  A poli sci major, I love anything  diplomacy, in theory as well as practice.  Eventually, I want to work for the State Department and serve as an IR professor somewhere.' + 
 				'<br><br>In my free time, if there is ever any, I enjoy hanging out with  friends and playing sports.  Most recently, I\'ve rediscovered my passion for soccer. I\'m terrible at it, but haven\'t given up.  As Vietnamese like to say, "Đá hay không bằng hay đá" (playing soccer often is better than just being good at it).  So hopefully, one of these days my wish to persist will reward me with a goal....or at least prevent me from scoring one in the wrong end :D'
 				+'<hr>Email me at <a href="mailto:mark.pham@yale.edu">mark.pham@yale.edu</a> '),
@@ -174,7 +169,7 @@ effect: function(offset) {
 	$(this).qtip({ 
 		content: { 
 			title: '<h2>Johnny Xu</h2>',
-			text: buildprofile('/static/img/dance.jpg', 'Chào bạn!' + 
+			text: buildprofile('/static/img/poppics/johnny.jpg', 'Chào bạn!' + 
 				'<br><br>You can call me Johnny. I spent 52% of my life in Dalian, China (complicated) where I was born, 20% in Canada (freedom), 18% in Seattle (most livable city), and 10% in New Orleans (warm, crawfish).'+
 				'<br><br>I\'m currently undecided about my major, but I enjoy learning economics and other social sciences very much. I enjoy playing basketball and making music. When the new series come out please watch it at "http://www.fox.com/24-live-another-day/" because it\'s my favorite show other than VISA\'s annual cultural show!' 
 				+'<hr>Let\'s chill at <a href="mailto:zhaoning.xu@yale.edu">zhaoning.xu@yale.edu</a> '),
@@ -201,9 +196,7 @@ effect: function(offset) {
 	$(this).qtip({ 
 		content: { 
 			title: '<h2>Minh Tri Pham</h2>',
-			text: buildprofile('/static/img/dance.jpg', 'Hello friends! <br><br> I’m Kevin (P.), ES ‘16, and I hail from the wild realm of Ohio, from a suburb devoid of much cultural diversity. When I’m not doing Asian things, you can find me playing in DPops and SYO, raising roundworms at the School of Medicine, crying over Kdramas, or getting fat from baked goods and ice cream. I also go to school.' + 
-				'<br><br>I got involved with ViSA last year to make up for this lack of culture in my life, and to obey my mother by making lots of “nice Vietnamese friends”. After a term as social chair, I now humbly serve as co-president of the most beautiful and talented cultural group on campus. I hope you’ll join me and the rest of ViSA in having a fun and delicious year!' +
-				'<br><br>See you at our next event (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧'
+			text: buildprofile('/static/img/poppics/minh.jpg', 'Yo! <br><br> I’m Minh and I come from the Czech Republic. My parents are Vietnamese, but I went to a German school in Prague. I am a programmer and designer and love working on useless (but super cool) projects. When I grow up, I want to become a tech entrepreneur, but right now I like watching Bayern Munich, learning new languages, drinking Pilsner Urquell, playing billiards, and betting on sports.'
 				+'<hr>Email me at <a href="mailto:minhtri.pham@yale.edu">minhtri.pham@yale.edu</a> '),
 			button: true
 		},
@@ -224,13 +217,16 @@ effect: function(offset) {
 			    	event: 'click'
 			    }
 			});
-} else if (position == 'social2') {
+} else if (position == 'social3') {
 	$(this).qtip({ 
 		content: { 
 			title: '<h2>Jason Liu</h2>',
-			text: buildprofile('/static/img/dance.jpg', 'Hello friends! <br><br> I’m Kevin (P.), ES ‘16, and I hail from the wild realm of Ohio, from a suburb devoid of much cultural diversity. When I’m not doing Asian things, you can find me playing in DPops and SYO, raising roundworms at the School of Medicine, crying over Kdramas, or getting fat from baked goods and ice cream. I also go to school.' + 
-				'<br><br>I got involved with ViSA last year to make up for this lack of culture in my life, and to obey my mother by making lots of “nice Vietnamese friends”. After a term as social chair, I now humbly serve as co-president of the most beautiful and talented cultural group on campus. I hope you’ll join me and the rest of ViSA in having a fun and delicious year!' +
-				'<br><br>See you at our next event (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧'
+			text: buildprofile('/static/img/poppics/jason.jpg', 
+				'Hi guys,<br><br> \
+				 I’m going to be one of the social chairs for ViSA this year. I come from Pearland, Texas. I like to take pictures of things, so don’t be alarmed if you catch me snapping away at you!\
+				  New kinds of food are always amazing and I also love watching intense movies.\
+				   Buzzfeed takes up 23.48% of my life. I’m excited for a great year full of FUN and CRAZY and CULTURAL activities!\
+				    <br><br>Shoot me an email if you want to chat or hang!'
 				+'<hr>Email me at <a href="mailto:jason.j.liu@yale.edu">jason.j.liu@yale.edu</a> '),
 			button: true
 		},
@@ -251,13 +247,11 @@ effect: function(offset) {
 			    	event: 'click'
 			    }
 			});
-} else if (position == 'social3') {
+} else if (position == 'social2') {
 	$(this).qtip({ 
 		content: { 
-			title: '<h2>Kevin A. Pham</h2>',
-			text: buildprofile('/static/img/dance.jpg', 'Hello friends! <br><br> I’m Kevin (P.), ES ‘16, and I hail from the wild realm of Ohio, from a suburb devoid of much cultural diversity. When I’m not doing Asian things, you can find me playing in DPops and SYO, raising roundworms at the School of Medicine, crying over Kdramas, or getting fat from baked goods and ice cream. I also go to school.' + 
-				'<br><br>I got involved with ViSA last year to make up for this lack of culture in my life, and to obey my mother by making lots of “nice Vietnamese friends”. After a term as social chair, I now humbly serve as co-president of the most beautiful and talented cultural group on campus. I hope you’ll join me and the rest of ViSA in having a fun and delicious year!' +
-				'<br><br>See you at our next event (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧'
+			title: '<h2>Kevin A. Nguyen</h2>',
+			text: buildprofile('/static/img/poppics/kevina.jpg', 'Hello friends! <br><br> I\'m Kevin and I am one of ViSA\'s social chairs'
 				+'<hr>Email me at <a href="mailto:kevin.a.nguyen@yale.edu">kevin.a.nguyen@yale.edu</a> '),
 			button: true
 		},
@@ -282,7 +276,7 @@ effect: function(offset) {
 	$(this).qtip({ 
 		content: { 
 			title: '<h2>Hannah Truong</h2>',
-			text: buildprofile('/static/img/dance.jpg', 'Chào mừng bạn đến trường đại học Yale!' + 
+			text: buildprofile('/static/img/poppics/hannah.jpg', 'Chào mừng bạn đến trường đại học Yale!' + 
 				'<br><br>My name is Hannah, and I am a freshman in Morse College hailing from Seattle, Washington. I hope to fulfill a science degree in biochemistry at Yale and continue on to medical school, with a particular interest in the business side of healthcare. '+
 				'<br><br>On campus, I am also involved in the Freshman Class Council and Morse College Council. I am a member of the Yale Cheerleading Squad (Go Bulldogs!), Kappa Kappa Gamma sorority and the Leadership Education and Development (LEAD) Program. '+
 				'<br><br>In my free time, I like to dance with the Yale Phoenix Asian Dance Team and cook food for my friends in the Morse student kitchen… Pho of course!'
@@ -310,9 +304,7 @@ effect: function(offset) {
 	$(this).qtip({ 
 		content: { 
 			title: '<h2>Gia Hoa Nguyen</h2>',
-			text: buildprofile('/static/img/dance.jpg', 'Hello friends! <br><br> I’m Kevin (P.), ES ‘16, and I hail from the wild realm of Ohio, from a suburb devoid of much cultural diversity. When I’m not doing Asian things, you can find me playing in DPops and SYO, raising roundworms at the School of Medicine, crying over Kdramas, or getting fat from baked goods and ice cream. I also go to school.' + 
-				'<br><br>I got involved with ViSA last year to make up for this lack of culture in my life, and to obey my mother by making lots of “nice Vietnamese friends”. After a term as social chair, I now humbly serve as co-president of the most beautiful and talented cultural group on campus. I hope you’ll join me and the rest of ViSA in having a fun and delicious year!' +
-				'<br><br>See you at our next event (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧'
+			text: buildprofile('/static/img/poppics/giahoa.png', 'Hello friends! <br><br> My name is Gia Hoa and I will be ViSA\'s treasurer for this year.'
 				+'<hr>Email me at <a href="mailto:giahoa.nguyen@yale.edu">giahoa.nguyen@yale.edu</a> '),
 			button: true
 		},
@@ -337,9 +329,7 @@ effect: function(offset) {
 	$(this).qtip({ 
 		content: { 
 			title: '<h2>Nancy Tran</h2>',
-			text: buildprofile('/static/img/dance.jpg', 'Hello friends! <br><br> I’m Kevin (P.), ES ‘16, and I hail from the wild realm of Ohio, from a suburb devoid of much cultural diversity. When I’m not doing Asian things, you can find me playing in DPops and SYO, raising roundworms at the School of Medicine, crying over Kdramas, or getting fat from baked goods and ice cream. I also go to school.' + 
-				'<br><br>I got involved with ViSA last year to make up for this lack of culture in my life, and to obey my mother by making lots of “nice Vietnamese friends”. After a term as social chair, I now humbly serve as co-president of the most beautiful and talented cultural group on campus. I hope you’ll join me and the rest of ViSA in having a fun and delicious year!' +
-				'<br><br>See you at our next event (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧'
+			text: buildprofile('/static/img/poppics/nancy.jpg', 'Hello friends! <br><br> My name is Nancy and I am the cultural chair for ViSA this year.'
 				+'<hr>Email me at <a href="mailto:nancy.tran@yale.edu">nancy.tran@yale.edu</a> '),
 			button: true
 		},
@@ -364,9 +354,7 @@ effect: function(offset) {
 	$(this).qtip({ 
 		content: { 
 			title: '<h2>Tony Nguyen</h2>',
-			text: buildprofile('/static/img/dance.jpg', 'Hello friends! <br><br> I’m Kevin (P.), ES ‘16, and I hail from the wild realm of Ohio, from a suburb devoid of much cultural diversity. When I’m not doing Asian things, you can find me playing in DPops and SYO, raising roundworms at the School of Medicine, crying over Kdramas, or getting fat from baked goods and ice cream. I also go to school.' + 
-				'<br><br>I got involved with ViSA last year to make up for this lack of culture in my life, and to obey my mother by making lots of “nice Vietnamese friends”. After a term as social chair, I now humbly serve as co-president of the most beautiful and talented cultural group on campus. I hope you’ll join me and the rest of ViSA in having a fun and delicious year!' +
-				'<br><br>See you at our next event (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧'
+			text: buildprofile('/static/img/poppics/tony.png', 'Hello friends! <br><br> My name is Tony and I am ViSA\'s historian. I am an passionate debater so I am mostly away on weekends.' 
 				+'<hr>Email me at <a href="mailto:tony.nguyen@yale.edu">tony.nguyen@yale.edu</a> '),
 			button: true
 		},
@@ -391,9 +379,7 @@ effect: function(offset) {
 	$(this).qtip({ 
 		content: { 
 			title: '<h2>Mimi Pham</h2>',
-			text: buildprofile('/static/img/dance.jpg', 'Hello friends! <br><br> I’m Kevin (P.), ES ‘16, and I hail from the wild realm of Ohio, from a suburb devoid of much cultural diversity. When I’m not doing Asian things, you can find me playing in DPops and SYO, raising roundworms at the School of Medicine, crying over Kdramas, or getting fat from baked goods and ice cream. I also go to school.' + 
-				'<br><br>I got involved with ViSA last year to make up for this lack of culture in my life, and to obey my mother by making lots of “nice Vietnamese friends”. After a term as social chair, I now humbly serve as co-president of the most beautiful and talented cultural group on campus. I hope you’ll join me and the rest of ViSA in having a fun and delicious year!' +
-				'<br><br>See you at our next event (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧'
+			text: buildprofile('/static/img/poppics/mimi.jpg', 'Hello friends! <br><br> I am ViSA\'s secretary'
 				+'<hr>Email me at <a href="mailto:mimi.pham@yale.edu">mimi.pham@yale.edu</a> '),
 			button: true
 		},
